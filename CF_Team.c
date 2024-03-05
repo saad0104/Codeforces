850d3fbff6b04d24c5b0bc73d3f686a07c691653
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-03-05 20:26:26 +0600

    Create CF_Team.c

---
+++ CF_Team.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z,count=0,solve=0,n;
+ 
+    scanf("%d",&n);
+ 
+    while(n>0){
+            count=0;
+ 
+    scanf("%d %d %d",&x,&y,&z);
+ 
+    if(x==1)
+    {
+        count++;
+    }
+    if(y==1)
+    {
+        count++;
+    }
+    if(z==1)
+    {
+        count++;
+    }
+    if(count>=2)
+    {
+        solve++;
+    }
+    n--;
+    }
+    printf("%d\n",solve);
+    return 0;
+}
