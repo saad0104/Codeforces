1223148a2b200b02cf1ae73ee0beddf2fbbf86e8
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-03-26 22:58:32 +0600

    Create CF_Fafa_and_His_Company.cpp

---
+++ CF_Fafa_and_His_Company.cpp
@@ -0,0 +1,19 @@
+//Fafa and his company
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, count = 0;
+    cin >> n;
+    for (int i = 1; i < n; i++)
+    {
+        if ((n - i) % i == 0)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
