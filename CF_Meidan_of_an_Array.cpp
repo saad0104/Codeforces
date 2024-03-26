ed9dca4d284d18ffb917bf93ccf338b7f255148e
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-03-26 20:18:30 +0600

    Create CF_Meidan_of_an_Array.cpp

---
+++ CF_Meidan_of_an_Array.cpp
@@ -0,0 +1,37 @@
+//Meidan of an Array
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n);
+        int m = ceil((n - 1) / 2);
+        //cout << arr[m];
+        int count = 0;
+        for (int i = m; i < n; i++)
+        {
+            if (arr[i] == arr[m])
+            {
+                count++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
