a513ab2949d3bed91ad2a1b9a4df6c2b85a47b51
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-03-29 23:24:41 +0600

    Create CF_Park_Lighting.cpp

---
+++ CF_Park_Lighting.cpp
@@ -0,0 +1,19 @@
+//Park Lighting
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int m, n, l;
+        cin >> m >> n;
+        l = ((n * m) + 1) / 2;
+        cout << l << endl;
+    }
+
+    return 0;
+}
