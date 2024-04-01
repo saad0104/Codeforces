ce610dcadc6b6e976a8118bb0a8487d4275f9103
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-04-01 20:46:40 +0600

    Create CF_Vus _the_Cossack_and_a_Contest.cpp

---
+++ CF_Vus _the_Cossack_and_a_Contest.cpp
@@ -0,0 +1,20 @@
+//Vus the Cossack and a Contest
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    if (n <= m && n <= k)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
