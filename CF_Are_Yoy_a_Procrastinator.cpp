d13bf2be41551b37a33bf1397bf222178cdca873
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-04-02 23:34:11 +0600

    Create CF_Are_Yoy_a_Procrastinator.cpp

---
+++ CF_Are_Yoy_a_Procrastinator.cpp
@@ -0,0 +1,12 @@
+// April Fools day Contest 2024
+// Are you a procrastinator
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string x;
+    cin >> x;
+    cout << "YES" << endl;
+    return 0;
+}
