be92676f45f1c3c32f4c4bd72f359bfbfd08a6a6
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-04-03 23:41:28 +0600

    Create CF_Arithmetic_Array.cpp

---
+++ CF_Arithmetic_Array.cpp
@@ -0,0 +1,36 @@
+// Arithmetic Array
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+
+            sum = sum + arr[i];
+        }
+
+        if (sum == n)
+        {
+            cout << "0" << endl;
+        }
+        else if (sum < n)
+        {
+            cout << "1" << endl;
+        }
+        else
+        {
+            cout << sum - n << endl;
+        }
+    }
+    return 0;
+}
