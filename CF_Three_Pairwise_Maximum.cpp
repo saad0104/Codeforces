05fb4db9f84a5e42274d2cbc356e4584dfb81d68
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-04-03 23:18:19 +0600

    Create CF_Three_Pairwise_Maximum.cpp

---
+++ CF_Three_Pairwise_Maximum.cpp
@@ -0,0 +1,32 @@
+// Three Pairwise Maximum
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        long long arr[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + 3);
+
+        if (arr[1] == arr[2])
+        {
+
+            cout << "YES" << endl;
+            cout << arr[0] << " " << arr[0] << " " << arr[2] << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
