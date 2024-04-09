e9bcc5230a61b3f7691584bff4c10d9c1be4e8ec
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-04-09 23:44:22 +0600

    Create CF_I_love_\%username\%1.cpp

---
+++ CF_I_love_\%username\%1.cpp
@@ -0,0 +1,35 @@
+// I_love_\%username\%1
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int count = 0;
+    int min = arr[0], max = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            count++;
+            max = arr[i];
+        }
+        else if (arr[i] < min)
+        {
+            count++;
+            min = arr[i];
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
