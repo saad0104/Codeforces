8b69a1836706cc9650d25b421ef5a1f2f24292fa
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-04-11 23:38:41 +0600

    Create CF_Buttons.cpp

---
+++ CF_Buttons.cpp
@@ -0,0 +1,43 @@
+// Buttons
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        long long a, b, c;
+        cin >> a >> b >> c;
+
+        if (c % 2 == 0)
+        {
+            if (a > b)
+            {
+                cout << "First" << endl;
+            }
+
+            else
+            {
+
+                cout << "Second" << endl;
+            }
+        }
+
+        else
+        {
+            if (b > a)
+            {
+
+                cout << "Second" << endl;
+            }
+            else
+            {
+                cout << "First" << endl;
+            }
+        }
+    }
+    return 0;
+}
