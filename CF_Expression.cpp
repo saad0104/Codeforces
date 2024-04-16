91b5640f54f0d6b8d6d66a5affe1b19feec2ed38
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-04-16 14:20:33 +0600

    Create CF_Expression.cpp

---
+++ CF_Expression.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int a,b,c;
+cin >> a >> b >> c;
+int y,z,m,n;
+n=a+b+c;
+y=(a+b)*c;
+z=a*(b+c);
+m=a*b*c;
+
+n=max(n,y);
+n=max(n,z);
+n=max(n,m);
+
+cout <<n<<endl;
+
+
+return 0;
+}
