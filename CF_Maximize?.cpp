0810d4b2eeb8df30a801d7a621655d76c492e698
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-05-05 08:33:21 +0600

    Create CF_Maximize?.cpp

---
+++ CF_Maximize?.cpp
@@ -0,0 +1,18 @@
+// Maximize?
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+
+        cout << x-1 << endl;
+        
+    }
+    return 0;
+}
