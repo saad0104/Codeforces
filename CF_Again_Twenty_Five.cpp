714011679fbcf7ba504923769b09ed53e77a7e24
saad0104 <155384440+saad0104@users.noreply.github.com>
2024-05-14 23:42:21 +0600

    Create CF_Again_Twenty_Five.cpp

---
+++ CF_Again_Twenty_Five.cpp
@@ -0,0 +1,13 @@
+// Again Twenty Five
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+  // int x = pow(5,n);
+    cout << "25" << endl;
+
+    return 0;
+}
