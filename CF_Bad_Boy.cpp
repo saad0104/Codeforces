42d5256c353e1d1405ece5b57ca705449e4a8428
Saad <155384440+saad0104@users.noreply.github.com>
2025-01-06 01:38:39 +0600

    Create CF_Bad_Boy.cpp

---
+++ CF_Bad_Boy.cpp
@@ -0,0 +1,18 @@
+// Bad Boy
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int m, n, i, j;
+        cin >> m >> n >> i >> j;
+
+        cout << 1 << " " << 1 << " " << m << " " << n << endl;
+    }
+
+    return 0;
+}
