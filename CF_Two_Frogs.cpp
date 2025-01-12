5494fea72cd74fef8af9d40824adf24da162aaed
Saad <155384440+saad0104@users.noreply.github.com>
2025-01-13 00:26:40 +0600

    Create CF_Two_Frogs.cpp

---
+++ CF_Two_Frogs.cpp
@@ -0,0 +1,25 @@
+// Two Frogs
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, a, b;
+        cin >> n >> a >> b;
+
+        if (abs(a - b) > 0 && abs(a - b) % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
